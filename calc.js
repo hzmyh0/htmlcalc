141c97708309573f185242a82731fe5a4888969e
Yohei  Hizume <hzmyh@hidzume-no-MacBook-Pro.local>
2015-05-30 09:02:56 +0900

    list traversing function

--- calc.js
+++ calc.js
@@ -4,4 +4,23 @@ var hcalc = {} || hcalc;
 hcalc.log = function() {
     console.log(arguments);
 }
+
+// traverse all elements of list.
+// if callback returns false, traverse will be canceled.
+// args 
+// list: list for traverse
+// callback: function to apply each elements of list
+//     callback args 
+//     element: element of list
+//     index: index of the element
+//     list: list being traversed
+//
+hcalc.each = function(list, callback) {
+    for (i = 0; i < list.length; i++) {
+        if(callback(list[i], i, list) === false) {
+            return false;
+        }
+    }
+    return true;
+}
     
