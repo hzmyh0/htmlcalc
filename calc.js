8a823ad5766d1e1cd8c8ccdbeb80aec601528dbb
Yohei  Hizume <hzmyh@hidzume-no-MacBook-Pro.local>
2015-05-30 23:58:55 +0900

    add listeners to digit buttons

--- calc.js
+++ calc.js
@@ -1,26 +1,41 @@
-var hcalc = {} || hcalc; 
+var hcalc = (function() {
+    // object for namespace
+    var exports = {} || exports; 
 
-// logs all arguments
-hcalc.log = function() {
-    console.log(arguments);
-}
-
-// traverse all elements of list.
-// if callback returns false, traverse will be canceled.
-// args 
-// list: list for traverse
-// callback: function to apply each elements of list
-//     callback args 
-//     element: element of list
-//     index: index of the element
-//     list: list being traversed
-//
-hcalc.each = function(list, callback) {
-    for (i = 0; i < list.length; i++) {
-        if(callback(list[i], i, list) === false) {
-            return false;
-        }
+    // ----- utility methods -----
+    // logs all arguments
+    exports.log = function() {
+        console.log(arguments);
     }
-    return true;
-}
+
+    // traverse the list and applies callback to all elements of the list.
+    // if callback returns false, traverse will be canceled.
+    // args 
+    // list: list for traverse
+    // callback: function to apply each elements of list
+    //     callback args 
+    //     element: element of list
+    //     index: index of the element
+    //     list: list being traversed
+    exports.each = function(list, callback) {
+        for (i = 0; i < list.length; i++) {
+            if(callback(list[i], i, list) === false) {
+                return false;
+            }
+        }
+        return true;
+    }
+    return exports;
+})();
     
+// ----- initializing -----
+document.addEventListener("DOMContentLoaded", function(){
+    var display = document.querySelector('input');
+    var buttons = document.querySelectorAll('button.digit');
+    var onClick = function(e) {
+        console.log(e.target.value);
+    };
+    hcalc.each(buttons, function(elem, index, list) {
+        elem.addEventListener('click', onClick, false);
+    });
+}, false);
